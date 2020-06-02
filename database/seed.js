6854e45f5c97bedbcaf8d5d08aa7ebb37bc018b5
David Lundholm <david.lundholm@zocom.se>
2020-06-02 12:37:46 +0200

    Added bcryptjs to seed

--- database/seed.js
+++ database/seed.js
@@ -2,11 +2,11 @@ const NeDB = require('nedb-promise')
 const uuid = require('uuid')
 const users = new NeDB({filename:'database/users.db', autoload: true})
 const products = new NeDB({filename:'database/products.db', autoload: true})
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const productsSeed = require('./productsSeed.json')
 const usersSeed = require('./usersSeed.json')
 
-usersSeed.forEach(user => user.password = bcrypt.hashSync('omg', 10));
+usersSeed.forEach(user => user.password = bcrypt.hashSync('password', 10));
 
 (async ()=>{
     await users.remove({},{multi:true})
