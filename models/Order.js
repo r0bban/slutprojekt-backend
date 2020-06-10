0b02f46b4921ad89ebda214e4c167c256e5526f8
r0bban <robert.p.svensson@gmail.com>
2020-06-10 21:56:00 +0200

    adjusted order in backend

--- models/Order.js
+++ models/Order.js
@@ -1,44 +1,70 @@
 const NeDB = require('nedb-promise')
-const orders = new NeDB({filename:'database/orders.db', autoload: true})
+const orders = new NeDB({ filename: 'database/orders.db', autoload: true })
 const Product = require('./Product')
 const User = require('./User')
 
-function validate(body){
-    let {items} = body
-    if(items){ return true }
+function validate(body) {
+    let { items } = body
+    if (items) { return true }
     return false
 }
 
+
+
+// const { items, customer, payment } = body;
+// const products = await Product.find(items.id);
+// const order = await orders.insert({
+//     item: { id: items.id, quantity: items.quantity },
+//     timeStamp: Date.now(),
+//     status: "inProcess",
+//     orderValue: products.reduce((acc, product) => acc + product.price, 0),
+// });
+
+
+// const { items, customer, payment } = body;
+//       const itemIds = items.map((x) => x.id);
+//       const products = await Product.find(itemIds);
+//       const order = await orders.insert({
+//         items,
+//         timeStamp: Date.now(),
+//         status: "inProcess",
+//         orderValue: products.reduce((acc, product) => acc + product.price, 0),
+//       });
+
+//This is create() is updated =>
 module.exports = {
-    async create(body, user){
-        if(validate(body)){
-            const {items, customer, payment} = body
-            const products = await Product.find(items)
+    async create(body, user) {
+        if (validate(body)) {
+            const { items, customer, payment } = body;
+            const itemIds = items.map((x) => x.id);
+            const products = await Product.find(itemIds);
             const order = await orders.insert({
                 items,
                 timeStamp: Date.now(),
-                status: 'inProcess',
-                orderValue: products.reduce((acc,product) => acc+product.price, 0)
-            })
+                status: "inProcess",
+                // orderValue: products.reduce((acc, product) => acc + product.price, 0),
+                orderValue: items.reduce(
+                    (acc, item, index) => acc + products[index].price * item.quantity,0),
+            });
             await User.addOrderToUser(order, user)
-            
 
-            return {error:false}
-        }else{
-            return {error:true}
+
+            return { error: false }
+        } else {
+            return { error: true }
 
         }
 
     },
 
-    async all(){
+    async all() {
         const ords = await orders.find({})
-        return ords.sort( (a,b) => a.timeStamp - b.timeStamp)
+        return ords.sort((a, b) => a.timeStamp - b.timeStamp)
     },
 
-    async findByCustomer(userID){
+    async findByCustomer(userID) {
         let user = await User.findOne(userID)
-        const ords = await orders.find({_id:{$in:user.orderHistory}})
+        const ords = await orders.find({ _id: { $in: user.orderHistory } })
         return ords
     }
 }
